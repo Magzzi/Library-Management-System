bf84e1be3474404722199983b7f56539ccce57fc
Mark Eron Diaz <markeron5@gmail.com>
2024-11-12 16:40:48 +0800

    Added personId attribute

--- src/classes/person.java
+++ src/classes/person.java
@@ -4,34 +4,52 @@ class Person {
     private String name;
     private int age;
     private String address;
+    private int personId;
 
+    
+    //Constructor w/ no parameters
+    public Person(){
+        System.out.println("Java");
+    }
+
+    //Constructor w/ parameters
     public Person(String name, int age, String address) {
         this.name = name;
         this.age = age;
         this.address = address;
     }
 
+    // Getter
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getAge() {
+            return age;
+        }
+
+    public String getAddress() {
+        return address;
     }
 
-    public int getAge() {
-        return age;
+    public int getPersonId(){
+        return personId;
+    }
+
+    //Setter
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
         this.age = age;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public void setPersonId(int personId){
+        this.personId = personId;
     }
 }
