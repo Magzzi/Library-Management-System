2b31962327e7fdf56c25bba73c2c30e4921b1032
Mark Eron Diaz <markeron5@gmail.com>
2024-11-25 23:16:40 +0800

    Added new table(users) for login and fixed the sql connection

--- src/database/database.java
+++ src/database/database.java
@@ -1,16 +1,19 @@
 import java.sql.*;
 
 public class database {
-  public static void main( String args[] ) {
+  public static Connection Connect( String args[] ) {
       Connection c = null;
       
-      try {
+      try{
          Class.forName("org.sqlite.JDBC");
          c = DriverManager.getConnection("jdbc:sqlite:library.db");
-      } catch ( Exception e ) {
+      }catch( Exception e ){
          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
          System.exit(0);
-      }
+      }finally{
       System.out.println("Opened database successfully");
+      }
+
+      return c;
    }
 }
